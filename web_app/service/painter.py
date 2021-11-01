665670db6e4602e5cf32646ee76b0b45a118eb62
iknoom <52124204+iknoom@users.noreply.github.com>
2021-11-01 22:23:44 +0900

    fix: update paint api

--- web_app/service/painter.py
+++ web_app/service/painter.py
@@ -5,11 +5,21 @@ from service.s3_connector import s3_connection, upload_image, download_image
 
 s3 = s3_connection()
 
-def paint_s3_image(upload_access_key, result_access_key):
+def paint_s3_image(reference_access_key, sketch_access_key, result_access_key):
     try:
-        download_image(s3, './upload.png', upload_access_key)
-        region_detect_skimage('./upload.png')
-        upload_image(s3, './result.png', result_access_key)
-    except:
-        return False
-    return True
+        # download image from s3
+        download_image(s3, './reference.png', reference_access_key)
+        download_image(s3, './sketch.png', sketch_access_key)
+        # do paint
+        region_detect_skimage('./sketch.png')
+        # upload result image to s3
+        resultUrl = upload_image(s3, './result.png', result_access_key)
+        return {
+            "resultUrl": resultUrl,
+            "success": True
+        }
+    except Exception as e:
+        print(e)
+        return {
+            "success": False
+        }
